676f62e139845ab6a95344ad0494bc3bc0a344df
Dhananjay <dhananjay.latkar@gmail.com>
2017-08-06 11:02:53 +0530

    sidebar added

--- index.php
+++ index.php
@@ -2,11 +2,29 @@
     <div class="pushbelow"></div>
     <div class="container-fluid text-center">
         <div class="row content">
+            <div class="col-sm-2 text-left">           
+                <ul class="sidebar-nav"> 
+                    <li>
+                        <a href="https://www.iitm.ac.in/">IITM</a>
+                    </li>
+                    <li>
+                        <a href="https://dost.iitm.ac.in/iitmdost/">DOST</a>
+                    </li>
+                    <li>
+                        <a href="https://www.facebook.com/saathi.iitm/">Facebook</a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com/reachiitm">Twitter</a>
+                    </li>
+                </ul>
+            </div>
+            <!--
             <div class="col-sm-2 sidenav">
                 <p><a href="#">Link</a></p>
                 <p><a href="#">Link</a></p>
                 <p><a href="#">Link</a></p>
             </div>
+            -->
             <div class="col-sm-8 text-left scrl">
             <h1><?php echo(($currpage?ucfirst($currpage):'Welcome'));?></h1>
                 <?php if(!@include('include/'.($currpage?$currpage:'welcome').'.html')) include('include/404.html'); ?>            
